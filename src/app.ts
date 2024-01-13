f30249ea38ae3f896f7d8b6b0030b59a5df6aba2
rybarczykchris <rybarczyk.chris@gmail.com>
2024-01-13 17:20:45 +0100

    feat: adds multer for handling multi-part uploads

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
-import express, {
-  type Request,
-  type Response,
-  type Application,
-} from "express";
+import express, { type Request, type Response, type Application } from "express";
 import dotenv from "dotenv";
+import { upload } from "./multer";
 
 dotenv.config();
 
@@ -12,6 +9,27 @@ const port = process.env.PORT ?? 8000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
+});
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     console.log("destination", "/tmp/uploads/");
+//     cb(null, "/tmp/uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     console.log(
+//       "filename",
+//       file.fieldname + "-" + Date.now() + "-" + file.originalname
+//     );
+//     cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+app.post("/upload", upload.single("file"), (req: Request, res: Response) => {
+  res.send("File uploaded successfully");
 });
 
 app.listen(port, () => {
