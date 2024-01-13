decd84ebd8055d110e83ded601a4fd04138970f1
rybarczykchris <rybarczyk.chris@gmail.com>
2024-01-13 15:36:40 +0100

    build: adds eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
-import express, { Request, Response , Application } from 'express';
-import dotenv from 'dotenv';
+import express, {
+  type Request,
+  type Response,
+  type Application,
+} from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT ?? 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
   console.log(`Server online at http://localhost:${port}`);
 });
-
